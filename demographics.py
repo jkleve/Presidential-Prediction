eeb76d8929170931d285ff11d3fa202c6b1bb374
Jesse Kleve <jesse.kleve@gmail.com>
2016-10-30 22:08:12 -0500

    added decision tree with accuracy testing

--- demographics.py
+++ demographics.py
@@ -1,11 +1,12 @@
 import csv
 import numpy as np
 import sexmachine.detector as gender
+from sklearn import tree
 import gender_detector
 import time
 import sys
 
-NUM_FEATURES = 11
+NUM_FEATURES = 10
 
 CANDIDATE_ID = 1
 CANDIDATE_NAME = 2
@@ -47,7 +48,8 @@ CANDIDATES = {
 
 # FEC file names
 FEC_FILES = {
-    'iowa':  'demographics/P00000001-IA.csv'
+    #'iowa':  'demographics/P00000001-IA.csv'
+    'iowa':  'P00000001-IA.csv'
 }
 
 ################################################
@@ -249,12 +251,40 @@ def get_features(filename):
                 data[i][7] = is_greater(amount, 500.0)
                 data[i][8] = is_greater(amount, 1000.0)
                 data[i][9] = zip_code
-                data[i][10] = is_male(name)
-                data[i][11] = get_candidate(candidate)
+                #data[i][10] = is_male(name)
+                data[i][NUM_FEATURES] = get_candidate(candidate)
             i += 1
             if i % n == 0:
                 print("%d%%" % int(float(i)/n*100))
 
+    return data
+
+################################################
+#
+# 
+#
+################################################
+def convert_to_nd_array(d):
+    n = len(d)
+    m = NUM_FEATURES
+    data = np.zeros(shape=(n,m+1)) # add 1 for y (output)
+    for s in range(0,n): # for each sample
+        for f in range(0,m+1): # for each feature
+            data[s][f] = d[s][f]
+    return data
+
+################################################
+#
+# Get all features from data file
+#
+################################################
+def load_features(filename):
+    data = []
+    with open(filename, 'r') as f:
+        for line in f:
+            tmp = line.rstrip().split(',')[:-1]
+            data.append(tmp)
+    data = convert_to_nd_array(data)
     return data
 
 ################################################
@@ -267,23 +297,90 @@ def save_features(filename, data):
     m = data.shape[1]
     with open(filename, 'w') as f:
         for i in range(0,n):
-            for j in range(0,m):
-                f.write("%d," % data[i][j])
-            f.write("\n")
+            if data[i][NUM_FEATURES] != 0:
+                for j in range(0,m):
+                    f.write("%d," % data[i][j])
+                f.write("\n")
+
+def get_test_sets(d, start, stop):
+    train = None
+    test = None
+
+    if stop > d.shape[0]:
+        print("Contraining end of test data")
+        stop = d.shape[0]
+
+    beginning = d[0:start]
+    end = d[stop+1:]
+    if len(beginning) > 0:
+        if len(end) > 0:
+            train =  (np.concatenate((beginning,end)))
+        else:
+            train =  beginning
+    else:
+        train = end
+
+    test = d[start:stop+1]
+
+    return (train, test)
+
+################################################
+#
+# 
+#
+################################################
+def test(data, subset_size=10):
+    accuracy = []
+    correct = 0
+    attempts = 0 
+
+    samples = data.shape[0]
+    test_size = int(float(samples)/subset_size) # TODO a little error in getting data subsets but...
+
+    for i in range(subset_size):
+        start = i*test_size
+        end = start + test_size
+        train, test = get_test_sets(data, start, end)
+        X = train[:,0:NUM_FEATURES]
+        y = train[:,-1]
+        clf_tree = tree.DecisionTreeClassifier()
+        clf_tree.fit(X,y)
+        test_data = test[:,0:NUM_FEATURES]
+        for i in range(0, test.shape[0]):
+            guess = clf_tree.predict(test_data[i])
+            if guess == test[i,-1]:
+                correct += 1
+            attempts += 1
+        accuracy.append(float(correct)/attempts)
+    accuracy = sum(accuracy) / len(accuracy)
+    return accuracy
 
 if __name__ == "__main__":
-    t0 = time.time() # get start time
+    #t0 = time.time() # get start time
+    #d = get_features(FEC_FILES['iowa'])
+    #save_features('IA.dat', d)
 
-    d = get_features("P00000001-IA.csv")
+    d = load_features("IA.dat")
+    print(test(d))
+    sys.exit()
 
-    # timing
-    t = time.time() - t0
-    h = int(t / 3600)
-    t -= h*3600
-    m = int(t / 60)
-    t -= m*60
-    s = int(t)
-    print("Time: %d hours %d minutes %d seconds" % (h, m, s)) 
+    X = d[:,0:NUM_FEATURES]
+    y = d[:,-1]
+    clf_tree = tree.DecisionTreeClassifier()
+    clf_tree.fit(X,y)
+    guess = clf_tree.predict([1,0,0,0,0,1,0,0,0,0])
+    
+    print(guess)
+    sys.exit()
+
+    ## timing
+    #t = time.time() - t0
+    #h = int(t / 3600)
+    #t -= h*3600
+    #m = int(t / 60)
+    #t -= m*60
+    #s = int(t)
+    #print("Time: %d hours %d minutes %d seconds" % (h, m, s)) 
 
     # save the features
     save_features('IA.dat', d)
